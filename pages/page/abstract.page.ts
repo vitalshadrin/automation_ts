b3968608d22fd0fb90a9a3d6d8bb6ede53cb1eab
vshadrin <vshadrin@exadel.com>
2020-08-25 00:54:20 +0300

    Added verify_element_by_text

--- pages/page/abstract.page.ts
+++ pages/page/abstract.page.ts
@@ -1,3 +1,4 @@
+import { expect } from "chai";
 export default class AbstractPage {
   click(element: WebdriverIO.Element): void {
     element.click();
@@ -8,5 +9,9 @@ export default class AbstractPage {
     value: string | number | boolean | object | any[]
   ): void {
     element.setValue(value);
+  }
+
+  verify_element_by_text(element: WebdriverIO.Element, expected: string): void {
+    expect(element.getText()).to.be.eq(expected);
   }
 }
